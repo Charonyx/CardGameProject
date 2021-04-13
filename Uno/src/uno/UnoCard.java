3c8cf87a9e4a7ee901bc497f1d96a332214393ce
Charonyx <churairat.onn@gmail.com>
2021-04-14 01:36:54 +0700

    add UnoCard

--- Uno/src/uno/UnoCard.java
+++ Uno/src/uno/UnoCard.java
@@ -10,5 +10,22 @@ package uno;
  * @author chura
  */
 public class UnoCard {
-    
+
+    enum color {
+        Red, Blue, Yellow, Green, Wild;
+        private static final Color[] color = Color.value();
+
+        public static Color getColor(int i) {
+            return Color.color[i];
+        }
+    }
+
+    enum value {
+        Zero, One, Two, Three, Four, Five, Six, Seven, Eight, Nine, DrawTwo, Reverse, Skip, WildFour;
+        private static final Value[] value = Value.value();
+
+        public static Value getValue(int i) {
+            return Value.value[i];
+        }
+    }
 }
